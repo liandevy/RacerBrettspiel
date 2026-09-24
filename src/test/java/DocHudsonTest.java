a3782d89b5bca358d94a78b3e7cc8998c31c4b50
AronF125 <aronfinks7@gmail.com>
2026-09-24 14:56:13 +0200

    Create DocHudsonTest.java
    Test erstellt für Aron's Auto
    - Auto kann nicht rückwärtsfahren

---
+++ src/test/java/DocHudsonTest.java
@@ -0,0 +1,14 @@
+import com.academy.DocHutson;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DocHudsonTest {
+    private DocHudson auto = new DocHudson();
+
+    @Test
+    void  resultsCanNotBeNegative() {
+        for (int i = 0;i < 10000; i++){
+           assertTrue(auto.fahreRunde()>=0);
+        }
+    }
+}
