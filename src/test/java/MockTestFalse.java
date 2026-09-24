32ca52fe6d0f43ccbbb06babed1c720d296be416
AronF125 <aronfinks7@gmail.com>
2026-09-24 13:21:18 +0200

    Update MockTestFalse.java

--- src/test/java/MockTestFalse.java
+++ src/test/java/MockTestFalse.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MockTest {
+class MockTestFalse {
 
     @Test
     void mul2Numbers() {
         assertEquals(8, 4 * 3, "Math not mathing");
     }
-
+}
 
