baeb9220204ffc4ad26b6a18d0f44c9cf146a5df
AronF125 <aronfinks7@gmail.com>
2026-09-24 16:05:43 +0200

    Update DocHudson.java

--- src/main/java/com/academy/DocHudson.java
+++ src/main/java/com/academy/DocHudson.java
@@ -3,7 +3,7 @@ package com.academy;
 public class DocHudson implements Auto {
     @Override
     public int fahreRunde() {
-        int zufall = RandomService.rangeInclusive(1, 10);
+        int zufall = RandomService.rangeInclusive(1, 12);
         return zufall;
     }
 }
