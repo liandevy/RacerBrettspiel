77065c68862d4f0eabe2606670781e13c603c0b6
TrueLeeL <lgerards75@gmail.com>
2026-09-24 14:11:42 +0200

    Create LeonAuto.java

---
+++ src/main/java/com/academy/LeonAuto.java
@@ -0,0 +1,24 @@
+package com.academy;
+
+public class LeonAuto implements Auto{
+    private static final int DEFAULT_BONUS = 0;
+
+    private final int bonus;
+
+    public LeonAuto() {
+        this(DEFAULT_BONUS);
+    }
+
+    public LeonAuto(final int bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public int fahreRunde() {
+        return RandomService.rangeInclusive(1, 4) + bonus;
+    }
+
+    public int getBonus(){
+        return bonus;
+    }
+}
