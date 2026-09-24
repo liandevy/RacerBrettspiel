816a1e69b532f5e26a3f2b8452589579da2dd1bf
Albayati-Hub <senior.yasso@gmail.com>
2026-09-24 14:12:30 +0200

    Yassir Auto

---
+++ src/main/java/com/academy/YassirAuto.java
@@ -0,0 +1,13 @@
+package com.academy;
+
+public class YassirAuto implements Auto {
+
+    @Override
+    public int fahreRunde() {
+
+        int wuerfel1 = RandomService.rangeInclusive(1, 4);
+        int wuerfel2 = RandomService.rangeInclusive(1, 4);
+
+        return wuerfel1 + wuerfel2;
+    }
+}
