311d3dfa2cc4daeacf51b945f777259018cf2ef9
Gianidev94 <lenders_85ware@icloud.com>
2026-09-25 10:11:37 +0200

    Änderungen
    Änderungen am Auto

---
+++ src/main/java/com/academy/autos/GianisAuto.java
@@ -0,0 +1,14 @@
+package com.academy.autos;
+
+import com.academy.RandomService;
+
+public class GianisAuto implements Auto {
+
+    @Override
+    public int fahreRunde() {
+
+        int wuerfel1 = RandomService.rangeInclusive(1, 14);
+
+        return wuerfel1;
+    }
+}
