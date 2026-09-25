f5a4b150d7cf0824c229d325b415e280933401fb
Gianidev94 <lenders_85ware@icloud.com>
2026-09-25 10:57:08 +0200

    Update GianisAuto.java

--- src/main/java/com/academy/autos/GianisAuto.java
+++ src/main/java/com/academy/autos/GianisAuto.java
@@ -7,7 +7,7 @@ public class GianisAuto implements Auto {
     @Override
     public int fahreRunde() {
 
-        int wuerfel1 = RandomService.rangeInclusive(1, 9);
+        int wuerfel1 = RandomService.rangeInclusive(1, 15);
 
         return wuerfel1;
     }
