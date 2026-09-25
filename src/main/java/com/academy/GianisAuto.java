01b7aef4964eb02fc2457b99facd259fe40b6e79
Gianidev94 <lenders_85ware@icloud.com>
2026-09-25 10:15:35 +0200

    Commit Auto änderung
    Test

--- src/main/java/com/academy/GianisAuto.java
+++ src/main/java/com/academy/GianisAuto.java
@@ -5,7 +5,7 @@ public class GianisAuto implements Auto{
     @Override
     public int fahreRunde() {
 
-        int wuerfel1 = RandomService.rangeInclusive(1, 13);
+        int wuerfel1 = RandomService.rangeInclusive(1, 16);
 
         return wuerfel1;
     }
