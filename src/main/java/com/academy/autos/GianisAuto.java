3c9d75c886917f50eed474450dbd1da80c6c7258
Gianidev94 <lenders_85ware@icloud.com>
2026-09-25 11:01:06 +0200

    Würfelanzahl Gianis Auto
    Anzahl auf 12 erhöht.

--- src/main/java/com/academy/autos/GianisAuto.java
+++ src/main/java/com/academy/autos/GianisAuto.java
@@ -7,7 +7,7 @@ public class GianisAuto implements Auto {
     @Override
     public int fahreRunde() {
 
-        int wuerfel1 = RandomService.rangeInclusive(1, 6);
+        int wuerfel1 = RandomService.rangeInclusive(1, 12);
 
         return wuerfel1;
     }
